5b24dfc2384ec12cbc73219a6d082822fc7cc8d7
Usman Malik <ussiimalik07@gmail.com>
2025-10-11 23:13:54 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/*
+ * Lab6Task1.cpp
+ * Created: 07/10/2025 10:27:45 pm
+ * Author : Usman
+ */ 
+
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
@@ -5,7 +11,7 @@
 #include "debug_prints.c"
 
 #define BAUD 115200
-#define MYUBRR ((F_CPU / (8UL * BAUD)) - 1)  
+#define MYUBRR ((F_CPU / (8UL * BAUD)) - 1)  // Double speed mode
 
 #define KEYPAD_DDR_ROWS DDRB
 #define KEYPAD_PORT_ROWS PORTB
@@ -14,11 +20,12 @@
 #define KEYPAD_PORT_COLS PORTD
 #define KEYPAD_PINS_COLS PIND
 
-unsigned char keypad_tbl2[] = {
-	'1','2','3','A',
-	'4','5','6','B',
-	'7','8','9','C',
-	'*','0','#','D'
+// Keypad character mapping
+unsigned char keypad_tbl2[16] = {
+	'7','8','9','/',    // Row A
+	'4','5','6','*',    // Row B
+	'1','2','3','-',    // Row C
+	'C','0','=','+'      // Row D
 };
 
 void UART_init(unsigned int ubrr);
@@ -29,81 +36,89 @@ unsigned char read_keypad(void);
 
 int main(void)
 {
-	unsigned char key_pressed = 0;
-	UART_init(MYUBRR);
+    UART_init(MYUBRR);
 
-	UART_send_string("MYUBRR= ");
-	UART_send_int(MYUBRR);
-	UART_send_string("\nF_CPU = ");
-	UART_send_int((int)(F_CPU / 1000000));
-	UART_send_string("\n\nPress a key:\n");
+    UART_send_string("MYUBRR= ");
+    UART_send_int(MYUBRR);
+    UART_send_string("\nF_CPU = ");
+    UART_send_int((int)(F_CPU / 1000000));
+    UART_send_string("\n\nPress a key:\n");
 
-	while (1)
-	{
-		key_pressed = read_keypad();
-		if (key_pressed != 0)
-		{
-			_delay_ms(200); 
-			UART_send_char(keypad_tbl2[key_pressed - 1]);
-			key_pressed = 0;
-		}
-	}
-	return 0;
+    while (1)
+    {
+        unsigned char key_index = read_keypad();
+        if (key_index != 255)
+        {
+            UART_send_char(keypad_tbl2[key_index]);
+        }
+    }
 }
 
+// Read 4x4 keypad
 unsigned char read_keypad()
 {
-	const unsigned char row_pins[4] = {0,1,2,3}; // PB0-PB3
-	const unsigned char col_pins[4] = {4,5,6,7}; // PD4-PD7
-	unsigned char r, c;
+    const unsigned char row_pins[4] = {0,1,2,3}; // PB0-PB3
+    const unsigned char col_pins[4] = {4,5,6,7}; // PD4-PD7
+    unsigned char r, c;
 
-	KEYPAD_DDR_ROWS &= 0xF0;  
-	KEYPAD_PORT_ROWS |= 0x0F; 
-	KEYPAD_DDR_COLS |= 0xF0;  
-	KEYPAD_PORT_COLS |= 0xF0; 
+    // Set rows as input with pull-ups
+    KEYPAD_DDR_ROWS &= 0xF0;
+    KEYPAD_PORT_ROWS |= 0x0F;
 
-	for (c = 0; c < 4; c++)
-	{
-		KEYPAD_PORT_COLS |= 0xF0;                
-		KEYPAD_PORT_COLS &= ~(1 << col_pins[c]); 
-		_delay_us(5);
+    // Set columns as outputs HIGH
+    KEYPAD_DDR_COLS |= 0xF0;
+    KEYPAD_PORT_COLS |= 0xF0;
 
-		for (r = 0; r < 4; r++)
-		{
-			if (!(KEYPAD_PINS_ROWS & (1 << row_pins[r])))
-			{
-				return r * 4 + c + 1;
-			}
-		}
-	}
-	return 0;
+    for (c = 0; c < 4; c++)
+    {
+        KEYPAD_PORT_COLS |= 0xF0;                 // Reset all HIGH
+        KEYPAD_PORT_COLS &= ~(1 << col_pins[c]); // Drive one column LOW
+        _delay_us(5);
+
+        for (r = 0; r < 4; r++)
+        {
+            if (!(KEYPAD_PINS_ROWS & (1 << row_pins[r])))
+            {
+                // Wait until key is released (debounce)
+                while (!(KEYPAD_PINS_ROWS & (1 << row_pins[r])))
+                {
+                    _delay_ms(10);
+                }
+                return r * 4 + c; // Return key index 0-15
+            }
+        }
+    }
+    return 255; // No key pressed
 }
 
+// UART initialization
 void UART_init(unsigned int ubrr)
 {
-	UBRR0H = (unsigned char)(ubrr >> 8);
-	UBRR0L = (unsigned char)ubrr;
-	UCSR0A |= (1 << U2X0);          
-	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
-	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); 
+    UBRR0H = (unsigned char)(ubrr >> 8);
+    UBRR0L = (unsigned char)ubrr;
+    UCSR0A |= (1 << U2X0);          // Double speed
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8-bit data, 1 stop bit
 }
 
+// Send a single character
 void UART_send_char(unsigned char data)
 {
-	while (!(UCSR0A & (1 << UDRE0)));
-	UDR0 = data;
+    while (!(UCSR0A & (1 << UDRE0)));
+    UDR0 = data;
 }
 
+// Send a string
 void UART_send_string(const char* str)
 {
-	while (*str)
-	UART_send_char(*str++);
+    while (*str)
+        UART_send_char(*str++);
 }
 
+// Send integer as string
 void UART_send_int(int data)
 {
-	char buffer[10];
-	itoa(data, buffer, 10);
-	UART_send_string(buffer);
+    char buffer[10];
+    itoa(data, buffer, 10);
+    UART_send_string(buffer);
 }
-
